8c9c38c481e080c5bfd2f69a7b886bc0f11e8f27
Nyi Htet <nyihtet4dev@icloud.com>
2023-03-02 15:23:58 -0800

    Initial Commit

---
+++ IMO/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  IMO
+//
+//  Created by Nyi Htet on 3/2/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
