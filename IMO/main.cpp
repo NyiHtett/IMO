bdbe0da32e52f2502689a8df770c4ffe0298fca1
Nyi Htet <nyihtet4dev@icloud.com>
2023-03-02 15:27:14 -0800

    first commit

--- IMO/main.cpp
+++ IMO/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  IMO
-//
-//  Created by Nyi Htet on 3/2/23.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+// second problem
+int factorial(int num){
+    if(num == 0){
+        return 1;
+    }
+    else{
+        return num * factorial(num-1);
+    }
+}
+
+bool isPrime(int num){
+    for(int i = 2; i < num; i++){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    for(int a = 0; a < 20; a++){
+        for(int b = 0; b < 20; b++){
+            for(int i = 2; i < 20 ; i++){
+                if(isPrime(i)){
+                    if(pow(a,i) == factorial(b) + i){
+                        cout << "a:" << a << " ";
+                        cout << "b:" << b << " ";
+                        cout << "p:" << i << endl;
+                    }
+                }
+            }
+        }
+    }
 }
